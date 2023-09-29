acefddb4a7612e413a62a92b3f2d4e7bed988c7c
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-29 10:52:24 +0200

    Add students ArrayList and add 3 students

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.school.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Aljoscha","Nyang","D12345"));
+        students.add(new Student("Fabian","Link","D24454"));
+        students.add(new Student("Dominic","Bergmann","D45345"));
+
+        for(Student s: students) {
+            System.out.println(s.getFirstName());
+        }
+
     }
 }
